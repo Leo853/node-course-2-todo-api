d6b0a773f611831d360bb6ab42d4cdf2e06ddff4
Leonid Trusov <leonid.trusov@kp.org>
2018-07-09 09:32:38 -0600

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -23,6 +24,26 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
+  }).catch( (e) => {
+    res.status(400).send(e);
+  });
+});
+
+// GET /todos/1235345
+app.get('/todos/:id', (req, res) => {
+  // res.send(req.params);
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+   return res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+    if (!todo){
+      return res.status(404).send()
+    }
+
+    res.send({todo});
   }).catch( (e) => {
     res.status(400).send(e);
   });
