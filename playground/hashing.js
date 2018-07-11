ced330ba63b65d7e2e465998af6d84997d0909d4
Leonid Trusov <leonid.trusov@kp.org>
2018-07-10 23:51:30 -0600

    Hash user pwd before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
+var password = '123abc!!';
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassword = '$2a$10$5DHuwgpohAttg3KWwI8rueHd8QeB2h8xhJL14YNH4T5LdaFsG/2Ja';
+bcrypt.compare(password, hashedPassword, (err,res) => {
+  console.log(res);
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded',decoded);
+// var data = {
+//   id: 10
+// }
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded',decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
